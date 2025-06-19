9820b360f8f11305c5442f12b79e994869609e85
Developer <dev@coicalc.local>
2025-06-19 07:58:43 -0300

    fix: Complete API to static data migration and clean up imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useRouter } from 'next/router';
 import { ObjectiveSelector, Resource } from '../components/ObjectiveSelector/ObjectiveSelector';
 import { RecipeSelector } from '../components/RecipeSelector/RecipeSelector';
-import { Recipe } from './api/recipes';
+import { Recipe } from '../utils/recipes';
 
 export default function Home() {
   const router = useRouter();
